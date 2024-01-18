42f56e23609c8b263efe5878093aaf7944075525
Stylan Roberts <stylanroberts@icloud.com>
2024-01-18 08:07:31 -0600

    first try on writing code for this

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,7 +1,22 @@
+import random
+
+
+
+
+
 # Create a welcome message that will be printed to the console. 
 
+print("---WELCOME TO THE GAME MY FRIEND---")
+
 # A random number should be chosen in the range 1-10.
+their_number = input("Pick a number between 1-10:    ")
+right_number = random.randint(1,10)
 
 # As a player of the game, I should be continuously prompted for a guess until I get it right.
+
+while their_number != right_number:
+    print("Ooooooo not quite, try again!!!    ")
+
+print(f"You got it!!! The number was{right_number}. Great job!!!")
 
 #  stuff
