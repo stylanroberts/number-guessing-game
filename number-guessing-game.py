c2afc244200d5a0d61cb7d0398bf3bc53895ad51
Stylan Roberts <stylanroberts@icloud.com>
2024-01-18 08:44:13 -0600

    almost final keeping comments for now

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,9 +1,6 @@
 import random
 
 
-
-
-
 # Create a welcome message that will be printed to the console. 
 
 print("---WELCOME TO THE GAME MY FRIEND---\n")
@@ -12,10 +9,11 @@ print("---WELCOME TO THE GAME MY FRIEND---\n")
 # A random number should be chosen in the range 1-10.
 
 right_number = random.randint(1,10)
-their_number = int(input("Pick a number between 1-10:    "))
+
 
 # As a player of the game, I should be continuously prompted for a guess until I get it right.
 
+their_number = int(input("Pick a number between 1-10:    "))
 
 while True:
     if their_number != right_number:
