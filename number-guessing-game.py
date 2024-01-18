e4cdd19e22e74c6cf0d3e163b8c8742766bdb890
Stylan Roberts <stylanroberts@icloud.com>
2024-01-18 09:15:04 -0600

    not done need to fix value error stuff

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -8,14 +8,30 @@ print("---WELCOME TO THE GAME MY FRIEND---\n")
 
 # A random number should be chosen in the range 1-10.
 
-right_number = random.randint(1,10)
-
+# right_number = random.randint(1,10)
+# their_number = int(input("Pick a number between 1-10:    "))
 
 # As a player of the game, I should be continuously prompted for a guess until I get it right.
 
-their_number = int(input("Pick a number between 1-10:    "))
+
+
+
+right_number = random.randint(1,10)
 
 while True:
+
+
+    print(right_number)
+
+    try:
+        their_number = int(input("Pick a number between 1-10:    "))
+    except ValueError:
+        their_number = int(input("I need a number between 1 and 10 please."))
+        
+
+    # except ValueError:
+    #     their_number = print("Please print a number.")
+
     if their_number != right_number:
         print("Ooooooo not quite, try again!!!    ")
         their_number = int(input("Pick another number between 1-10:    "))
