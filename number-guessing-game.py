21bd6cea10f169eb15110738c1c302d7e1cc75c7
Stylan Roberts <stylanroberts@icloud.com>
2024-01-18 08:42:22 -0600

    fixed a bunch it wasn't looping right

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -8,15 +8,20 @@ import random
 
 print("---WELCOME TO THE GAME MY FRIEND---\n")
 
+
 # A random number should be chosen in the range 1-10.
-their_number = input("Pick a number between 1-10:    ")
+
 right_number = random.randint(1,10)
+their_number = int(input("Pick a number between 1-10:    "))
 
 # As a player of the game, I should be continuously prompted for a guess until I get it right.
 
-if their_number != right_number:
-    print("Ooooooo not quite, try again!!!    ")
-    print(right_number)
-    their_number = input("Pick another number between 1-10:    ")
 
-print(f"You got it!!! The number was {right_number}. Great job!!!")
+while True:
+    if their_number != right_number:
+        print("Ooooooo not quite, try again!!!    ")
+        their_number = int(input("Pick another number between 1-10:    "))
+        
+    else:
+        print(f"You got it!!! The number was {right_number}. Great job!!!")
+        break
